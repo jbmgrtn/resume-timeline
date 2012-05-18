9966273f7951d1981871e0693f1ffdb5e0abd71e
Julie Pagano <julie.pagano@gmail.com>
2012-05-18 19:43:11 -0400

    Adds the ResumeTimeline object to the element data

--- spec/ResumeTimelineSpec.js
+++ spec/ResumeTimelineSpec.js
@@ -1,5 +1,15 @@
 describe("ResumeTimeline", function() {
-  it("test", function() {
+  var plugin_name = "plugin_resumeTimeline";
+
+  beforeEach(function() {
     loadFixtures('fixture.html');
   });
+
+  describe("init", function() {
+    it("adds the ResumeTimeline object to the element data", function() {
+      $("#resume-timeline").resumeTimeline();
+      expect($("#resume-timeline").data(plugin_name)).not.toBeUndefined();
+    });
+  });
+
 });
