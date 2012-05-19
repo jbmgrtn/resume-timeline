c8a300ff1a84b776fb98520cdf2ecc3fd9cb0d80
Julie Pagano <julie.pagano@gmail.com>
2012-05-18 23:48:32 -0400

    DrawTimeline draws a horizontal line

--- spec/ResumeTimelineSpec.js
+++ spec/ResumeTimelineSpec.js
@@ -44,15 +44,21 @@ describe("ResumeTimeline", function() {
     });
 
     it("creates paper for drawing", function() {
+      spyOn(resume_timeline, "drawTimeline");
       spyOn(resume_timeline, "createPaper");
       resume_timeline.draw();
-
       expect(resume_timeline.createPaper).toHaveBeenCalled();
     });
 
     it("sets paper value", function() {
       resume_timeline.draw();
       expect(resume_timeline.paper).not.toBeUndefined();
+    });
+
+    it("draws the timeline", function() {
+      spyOn(resume_timeline, "drawTimeline");
+      resume_timeline.draw();
+      expect(resume_timeline.drawTimeline).toHaveBeenCalled();
     });
   });
 
@@ -72,4 +78,68 @@ describe("ResumeTimeline", function() {
       expect(resume_timeline.paper.width).toEqual(container.width());
     });
   });
+
+  describe("drawTimeline", function() {
+    var resume_timeline;
+
+    beforeEach(function() {
+      container.resumeTimeline({ autoDraw: false});
+      resume_timeline = container.data(plugin_name);
+      resume_timeline.createPaper();
+    });
+
+    it("creates a horizontal line", function() {
+      spyOn(resume_timeline, "drawHorizontalLine");
+      resume_timeline.drawTimeline();
+      expect(resume_timeline.drawHorizontalLine).toHaveBeenCalled();
+    });
+
+    it("positions the line at 10,10", function() {
+      var spy = spyOn(resume_timeline, "drawHorizontalLine");
+      resume_timeline.drawTimeline();
+      expect(spy.mostRecentCall.args[0]).toEqual(10);
+      expect(spy.mostRecentCall.args[1]).toEqual(10);
+    });
+
+    it("makes the line 20 smaller than the paper width", function() {
+      var width = container.width() - 20;
+
+      var spy = spyOn(resume_timeline, "drawHorizontalLine");
+      resume_timeline.drawTimeline();
+      expect(spy.mostRecentCall.args[2]).toEqual(width);
+    });
+  });
+
+  describe("drawHorizontalLine", function() {
+    var resume_timeline;
+
+    beforeEach(function() {
+      container.resumeTimeline({ autoDraw: false});
+      resume_timeline = container.data(plugin_name);
+      resume_timeline.createPaper();
+    });
+
+    it("draws a path", function() {
+      spyOn(resume_timeline.paper, "path")
+      resume_timeline.drawHorizontalLine();
+      expect(resume_timeline.paper.path).toHaveBeenCalled();
+    });
+
+    it("moves to the specified coordinates", function() {
+      var x = 10;
+      var y = 10;
+      var spy = spyOn(resume_timeline.paper, "path")
+      resume_timeline.drawHorizontalLine(x, y);
+      expect(spy.mostRecentCall.args[0]).toContain("M10,10");
+    });
+
+    it("has the specified width", function() {
+      var width = 100;
+      var spy = spyOn(resume_timeline.paper, "path")
+      resume_timeline.drawHorizontalLine(10, 10, width);
+      expect(spy.mostRecentCall.args[0]).toContain("H" + width);
+    });
+  });
 });
+
+
