902a823c0f642defca664fc06ac901fe691a9ee0
Julie Pagano <julie.pagano@gmail.com>
2012-05-18 22:15:36 -0400

    Improve test coverage for creatPaper

--- spec/ResumeTimelineSpec.js
+++ spec/ResumeTimelineSpec.js
@@ -16,7 +16,34 @@ describe("ResumeTimeline", function() {
     it("creates paper for drawing", function() {
       $("#resume-timeline").resumeTimeline();
       var resume_timeline = $("#resume-timeline").data(plugin_name);
+      spyOn(resume_timeline, "createPaper");
+      resume_timeline.init();
+
+      expect(resume_timeline.createPaper).toHaveBeenCalled();
+    });
+
+    it("sets paper value", function() {
+      $("#resume-timeline").resumeTimeline();
+      var resume_timeline = $("#resume-timeline").data(plugin_name);
       expect(resume_timeline.paper).not.toBeUndefined();
+    });
+
+  });
+
+  describe("createPaper", function() {
+    var resume_timeline;
+
+    beforeEach(function() {
+      $("#resume-timeline").resumeTimeline();
+      resume_timeline = $("#resume-timeline").data(plugin_name);
+    });
+
+    it("sets the paper height to 100%", function() {
+      expect(resume_timeline.paper.height).toEqual("100%");
+    });
+
+    it("sets the paper width to 100%", function() {
+      expect(resume_timeline.paper.width).toEqual("100%");
     });
   });
 
