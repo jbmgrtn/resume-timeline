06b6ec2cc2744041a29c75689ecd8432a23b0a5f
Julie Pagano <julie.pagano@gmail.com>
2012-05-18 22:23:35 -0400

    Option autoDraw determines if the timeline is automatically drawn

--- spec/ResumeTimelineSpec.js
+++ spec/ResumeTimelineSpec.js
@@ -8,26 +8,50 @@ describe("ResumeTimeline", function() {
   describe("plugin", function() {
     it("adds the ResumeTimeline object to the element data", function() {
       $("#resume-timeline").resumeTimeline();
+      var resume_timeline = $("#resume-timeline").data(plugin_name);
       expect($("#resume-timeline").data(plugin_name)._name).toEqual("resumeTimeline");
     });
   });
 
   describe("init", function() {
-    it("creates paper for drawing", function() {
-      $("#resume-timeline").resumeTimeline();
-      var resume_timeline = $("#resume-timeline").data(plugin_name);
-      spyOn(resume_timeline, "createPaper");
-      resume_timeline.init();
-
-      expect(resume_timeline.createPaper).toHaveBeenCalled();
-    });
-
-    it("sets paper value", function() {
+    it("draws the timeline if the autoDraw function is not set", function() {
       $("#resume-timeline").resumeTimeline();
       var resume_timeline = $("#resume-timeline").data(plugin_name);
       expect(resume_timeline.paper).not.toBeUndefined();
     });
 
+    it("draws the timeline if the autoDraw function is set to true", function() {
+      $("#resume-timeline").resumeTimeline({autoDraw: true});
+      var resume_timeline = $("#resume-timeline").data(plugin_name);
+      expect(resume_timeline.paper).not.toBeUndefined();
+    });
+
+    it("does not draw the timeline if the autoDraw function is false", function() {
+      $("#resume-timeline").resumeTimeline({autoDraw: false});
+      var resume_timeline = $("#resume-timeline").data(plugin_name);
+      expect(resume_timeline.paper).toBeUndefined();
+    });
+  });
+
+  describe("draw", function() {
+    var resume_timeline;
+
+    beforeEach(function() {
+      $("#resume-timeline").resumeTimeline({ autoDraw: false});
+      resume_timeline = $("#resume-timeline").data(plugin_name);
+    });
+
+    it("creates paper for drawing", function() {
+      spyOn(resume_timeline, "createPaper");
+      resume_timeline.draw();
+
+      expect(resume_timeline.createPaper).toHaveBeenCalled();
+    });
+
+    it("sets paper value", function() {
+      resume_timeline.draw();
+      expect(resume_timeline.paper).not.toBeUndefined();
+    });
   });
 
   describe("createPaper", function() {
