80d7f5ec94f80332ae31aec108aa06216b95351c
Julie Pagano <julie.pagano@gmail.com>
2012-05-18 22:45:34 -0400

    Refactored tests

--- spec/ResumeTimelineSpec.js
+++ spec/ResumeTimelineSpec.js
@@ -1,34 +1,36 @@
 describe("ResumeTimeline", function() {
   var plugin_name = "plugin_resumeTimeline";
+  var container;
 
   beforeEach(function() {
     loadFixtures('fixture.html');
+    container = $("#resume-timeline");
   });
 
   describe("plugin", function() {
     it("adds the ResumeTimeline object to the element data", function() {
-      $("#resume-timeline").resumeTimeline();
-      var resume_timeline = $("#resume-timeline").data(plugin_name);
+      container.resumeTimeline();
+      var resume_timeline = container.data(plugin_name);
       expect($("#resume-timeline").data(plugin_name)._name).toEqual("resumeTimeline");
     });
   });
 
   describe("init", function() {
     it("draws the timeline if the autoDraw function is not set", function() {
-      $("#resume-timeline").resumeTimeline();
-      var resume_timeline = $("#resume-timeline").data(plugin_name);
+      container.resumeTimeline();
+      var resume_timeline = container.data(plugin_name);
       expect(resume_timeline.paper).not.toBeUndefined();
     });
 
     it("draws the timeline if the autoDraw function is set to true", function() {
-      $("#resume-timeline").resumeTimeline({autoDraw: true});
-      var resume_timeline = $("#resume-timeline").data(plugin_name);
+      container.resumeTimeline({autoDraw: true});
+      var resume_timeline = container.data(plugin_name);
       expect(resume_timeline.paper).not.toBeUndefined();
     });
 
     it("does not draw the timeline if the autoDraw function is false", function() {
-      $("#resume-timeline").resumeTimeline({autoDraw: false});
-      var resume_timeline = $("#resume-timeline").data(plugin_name);
+      container.resumeTimeline({autoDraw: false});
+      var resume_timeline = container.data(plugin_name);
       expect(resume_timeline.paper).toBeUndefined();
     });
   });
@@ -37,8 +39,8 @@ describe("ResumeTimeline", function() {
     var resume_timeline;
 
     beforeEach(function() {
-      $("#resume-timeline").resumeTimeline({ autoDraw: false});
-      resume_timeline = $("#resume-timeline").data(plugin_name);
+      container.resumeTimeline({ autoDraw: false});
+      resume_timeline = container.data(plugin_name);
     });
 
     it("creates paper for drawing", function() {
@@ -56,12 +58,10 @@ describe("ResumeTimeline", function() {
 
   describe("createPaper", function() {
     var resume_timeline;
-    var container;
 
     beforeEach(function() {
-      container = $("#resume-timeline");
       container.resumeTimeline();
-      resume_timeline = $("#resume-timeline").data(plugin_name);
+      resume_timeline = container.data(plugin_name);
     });
 
     it("sets the paper height to the height of the container", function() {
@@ -72,5 +72,4 @@ describe("ResumeTimeline", function() {
       expect(resume_timeline.paper.width).toEqual(container.width());
     });
   });
-
 });
