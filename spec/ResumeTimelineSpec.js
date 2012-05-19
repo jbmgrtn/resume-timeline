18fee3221cd25ade373eda81a057f716968f258b
Julie Pagano <julie.pagano@gmail.com>
2012-05-18 21:49:25 -0400

    Create paper for drawing

--- spec/ResumeTimelineSpec.js
+++ spec/ResumeTimelineSpec.js
@@ -5,10 +5,18 @@ describe("ResumeTimeline", function() {
     loadFixtures('fixture.html');
   });
 
-  describe("init", function() {
+  describe("plugin", function() {
     it("adds the ResumeTimeline object to the element data", function() {
       $("#resume-timeline").resumeTimeline();
-      expect($("#resume-timeline").data(plugin_name)).not.toBeUndefined();
+      expect($("#resume-timeline").data(plugin_name)._name).toEqual("resumeTimeline");
+    });
+  });
+
+  describe("init", function() {
+    it("creates paper for drawing", function() {
+      $("#resume-timeline").resumeTimeline();
+      var resume_timeline = $("#resume-timeline").data(plugin_name);
+      expect(resume_timeline.paper).not.toBeUndefined();
     });
   });
 
